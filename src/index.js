7a4bbb2b7a7b56a06e5fab5fde01c35528b19f87
Dimitrije108 <126205228+Dimitrije108@users.noreply.github.com>
2024-06-22 17:37:21 +0200

    feat: add retrieve gif API funcionality via button

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 // eslint-disable-next-line no-unused-vars
 import html from './index.html';
 import './style.css';
+
+const img = document.querySelector('img');
+const fetchButton = document.querySelector('.fetch-button');
+
+const getGif = () => {
+  fetch(
+    'https://api.giphy.com/v1/gifs/translate?api_key=tc9YKXRZLVTOZ2yw9L0nDU9zCpWXdHRi&s=star%20wars',
+    { mode: 'cors' }
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      img.src = response.data.images.original.url;
+    });
+};
+
+getGif();
+
+fetchButton.addEventListener('click', () => {
+  getGif();
+});
