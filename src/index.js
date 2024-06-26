4aba4002dff6d287c6c476464944b64d8cdbdd35
Dimitrije108 <126205228+Dimitrije108@users.noreply.github.com>
2024-06-26 15:45:48 +0200

    feat: add search functionality for Giphy API with basic error handling

--- src/index.js
+++ src/index.js
@@ -2,24 +2,60 @@
 import html from './index.html';
 import './style.css';
 
-const img = document.querySelector('img');
 const fetchButton = document.querySelector('.fetch-button');
+const img = document.querySelector('img');
+const errorMsg = document.querySelector('.error-msg');
+
+const getData = () => {
+  const inputVal = document.querySelector('#search-gif').value;
+  let encoded = encodeURI(inputVal);
+  return encoded;
+};
+
+const handleResponse = (response) => {
+  img.src = response.data.images.original.url;
+};
+
+const handleError = (error) => {
+  if (getData() === '') {
+    errorMsg.textContent = "You've searched nothing";
+    img.src =
+      'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExMjFhdnc2Zmdzems0a2RnNnRpYjFhdnQwbmxkdTY4cHk5dWk4b2pnYyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/f4DGnGf6xwFonJUI0D/giphy.gif';
+  } else {
+    errorMsg.textContent = error;
+    img.src =
+      'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExcGl1NzFmb2Z5MWl2MzF6NDAzdDdmaHR1ZjRwcndubWRoZHNpenRoNiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/vKz8r5aTUFFJu/giphy.gif';
+  }
+};
 
 const getGif = () => {
   fetch(
-    'https://api.giphy.com/v1/gifs/translate?api_key=tc9YKXRZLVTOZ2yw9L0nDU9zCpWXdHRi&s=star%20wars',
+    `https://api.giphy.com/v1/gifs/translate?api_key=tc9YKXRZLVTOZ2yw9L0nDU9zCpWXdHRi&s=${getData()}`,
     { mode: 'cors' }
   )
-    .then(function (response) {
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
       return response.json();
     })
-    .then(function (response) {
-      img.src = response.data.images.original.url;
+    .then((response) => {
+      console.log(response.data);
+      if (response.data.length === 0) {
+        throw new Error('GIF Not Found');
+      }
+      handleResponse(response);
+    })
+    .catch((error) => {
+      handleError(error);
     });
 };
 
-getGif();
+const clearError = () => {
+  errorMsg.textContent = '';
+};
 
 fetchButton.addEventListener('click', () => {
+  clearError();
   getGif();
 });
